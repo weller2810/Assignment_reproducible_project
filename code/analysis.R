89a7a61760e0d7523b7c3cf09c60e34534148912
chainsawriot <chainsawtiney@gmail.com>
2026-07-28 13:54:47 +0200

    Modify the R code so that it can output color figures

--- code/analysis.R
+++ code/analysis.R
@@ -30,22 +30,27 @@ png(
   height = 600
 )
 
+continents <- unique(summary_data$continent)
+## A color-blind safe color palette
+okabe_ito <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442",
+               "#0072B2", "#D55E00", "#CC79A7", "#000000")
+
 plot(
-  summary_data$year[summary_data$continent == "Europe"],
-  summary_data$lifeExp[summary_data$continent == "Europe"],
+  summary_data$year[summary_data$continent == continents[1]],
+  summary_data$lifeExp[summary_data$continent == continents[1]],
   type = "l",
   ylim = range(summary_data$lifeExp),
   xlab = "Year",
   ylab = "Average life expectancy",
-  main = "Average Life Expectancy by Continent"
+  main = "Average Life Expectancy by Continent",
+  col = okabe_ito[1]
 )
 
-continents <- unique(summary_data$continent)
-
-for (continent in continents) {
+for (i in seq_along(continents)) {
   lines(
-    summary_data$year[summary_data$continent == continent],
-    summary_data$lifeExp[summary_data$continent == continent]
+    summary_data$year[summary_data$continent == continents[i]],
+    summary_data$lifeExp[summary_data$continent == continents[i]],
+    col = okabe_ito[i]
   )
 }
 
@@ -53,7 +58,8 @@ legend(
   "bottomright",
   legend = continents,
   lty = 1,
-  cex = 0.8
+  cex = 0.8,
+  col = okabe_ito[seq_along(continents)]
 )
 
 dev.off()
